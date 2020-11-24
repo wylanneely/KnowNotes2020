adc349d76d2836b9b3399d6b36dd5674604eee63
Wylan Neely <wylanneely@gmail.com>
2020-11-24 03:26:26 -0700

    Adding Saxaphone Instrument
    -added to achievements & leaderboards controller
    -created model
    -added sounds
    added image

---
+++ AchievementsManager.swift
@@ -0,0 +1,115 @@
+//
+//  AchievementsManager.swift
+//  Know Notes
+//
+//  Created by Wylan L Neely on 10/18/20.
+//
+
+
+import GameKit
+
+
+ class AchievementsManager {
+    
+    
+    var playerAchievements: [GKAchievement]?
+    
+    private var userDefaultsHelper = LeaderboardsManager()
+    
+    //MARK: Achievements
+    
+    
+    let acousticGuitarAchievement = GKAchievement(identifier: AchievementsBundleIDs.unlockAcousticGuitar.rawValue)
+    let violinAchievement = GKAchievement(identifier: AchievementsBundleIDs.unlockViolin.rawValue)
+    let saxaphoneAchievement = GKAchievement(identifier: AchievementsBundleIDs.unlockSaxaphone.rawValue)
+
+    
+    var isAcousticGuitarUnlocked: Bool = false
+    var isViolinUnlocked: Bool = false
+    var isSaxUnlocked: Bool = false
+
+
+    
+    typealias SuccessHandler = (Bool) -> Void
+
+    func loadAchievements(sucess: SuccessHandler) {
+        var didLoad: Bool = false
+        GKAchievement.loadAchievements { (achievements, error) in
+            if let achievements = achievements {
+                self.playerAchievements = achievements
+                self.sortCompletedAchievements()
+                didLoad = true
+            } else {
+                didLoad = false
+            }
+        }
+        sucess(didLoad)
+    }
+    
+    func sortCompletedAchievements(){
+        if let pAchievements = playerAchievements {
+            for achievement in pAchievements {
+                switch achievement.identifier {
+                case AchievementsBundleIDs.unlockAcousticGuitar.rawValue :
+                    self.isAcousticGuitarUnlocked = achievement.isCompleted
+                case AchievementsBundleIDs.unlockViolin.rawValue :
+                    self.isViolinUnlocked = achievement.isCompleted
+                case AchievementsBundleIDs.unlockSaxaphone.rawValue :
+                    self.isSaxUnlocked = achievement.isCompleted
+                default:
+                    return } }
+        }
+    }
+    
+    func reportSaxaphoneProgress(with score: Int) {
+        if score >= 20 {
+            saxaphoneAchievement.percentComplete = 100.00
+            reportAchievement(saxaphoneAchievement)
+            userDefaultsHelper.unlockSaxLocally()
+        } else {
+           let completePercent =  Double(score) / 20
+            saxaphoneAchievement.percentComplete = completePercent
+            reportAchievement(saxaphoneAchievement)
+        }
+    }
+    
+    func reportViolinProgress(with score: Int) {
+        if score >= 20 {
+            violinAchievement.percentComplete = 100.00
+            reportAchievement(violinAchievement)
+            userDefaultsHelper.unlockViolinLocally()
+        } else {
+           let completePercent =  Double(score) / 20
+            violinAchievement.percentComplete = completePercent
+            reportAchievement(violinAchievement)
+        }
+    }
+    
+    func reportUnlockAcousticGuitarProgress(with score: Int) {
+        if score >= 20 {
+            acousticGuitarAchievement.percentComplete = 100.00
+            reportAchievement(acousticGuitarAchievement)
+            userDefaultsHelper.unlockAcousticGuitarLocally()
+        } else {
+           let completePercent =  Double(score) / 20
+            acousticGuitarAchievement.percentComplete = completePercent
+            reportAchievement(acousticGuitarAchievement)
+        }   
+    }
+    
+    
+    
+    func reportAchievement(_ achievement: GKAchievement) {
+        GKAchievement.report([achievement]) {  error in
+            if let error = error {
+            print("\(error)") }
+        }
+    }
+
+}
+enum AchievementsBundleIDs: String {
+    case unlockAcousticGuitar = "com.wylan.KnowYourNote2020.Achievements.AcousticGuitar"
+    case unlockViolin = "com.wylan.KnowYourNote2020.Achievements.Violin"
+    case unlockSaxaphone = "com.wylan.KnowYourNote2020.Achievements.Sax"
+
+}
--- LeaderboardsManager.swift
+++ LeaderboardsManager.swift
@@ -22,34 +22,169 @@ struct LeaderboardsManager {
     
     let defaults = UserDefaults.standard
     
+    func getHighScoreGrandPiano() -> Int {
+        return defaults.integer(forKey: kHighScoreGrandPiano)
+    }
+    
+    func highScoreAcousticGuitar() -> Int {
+        return defaults.integer(forKey: kHighScoreAGuitar)
+    }
+    
+    func highScoreViolin() -> Int {
+        return defaults.integer(forKey: kHighScoreViolin)
+    }
+    
+    func highScoreSax() -> Int {
+        return defaults.integer(forKey: kHighScoreSaxaphone)
+    }
+    //MARK: Instrument helper
+    
+    var isAcousticGuitarUnlocked: Bool {
+        if defaults.bool(forKey: kIsAcousticUnlocked) == true {
+            return true
+        } else {
+            return GameCenterManager.manager.achievementsManager.isAcousticGuitarUnlocked
+        }
+    }
+    
+    var isViolinUnlocked: Bool  {
+        if defaults.bool(forKey: kIsViolinUnlocked) == true {
+            return true
+        } else {
+            return GameCenterManager.manager.achievementsManager.isViolinUnlocked
+        }
+    }
+    var isSaxaphoneUnlocked: Bool  {
+        if defaults.bool(forKey: kIsSaxaphoneUnlocked) == true {
+            return true
+        } else {
+            return GameCenterManager.manager.achievementsManager.isSaxUnlocked
+        }
+    }
+    
+    func unlockAcousticGuitarLocally(){
+        defaults.setValue(true, forKey: kIsAcousticUnlocked)
+    }
+    func  unlockViolinLocally(){
+        defaults.setValue(true, forKey: kIsViolinUnlocked)
+    }
+    func  unlockSaxLocally(){
+        defaults.setValue(true, forKey: kIsViolinUnlocked)
+    }
+    
+    //MARK: check if round isFinished
+    
     var didFinishGrandPianoRound1: Bool {
         return defaults.bool(forKey: kGrandPianoRound1)
     }
     var didFinishGrandPianoRound2: Bool {
         return defaults.bool(forKey: kGrandPianoRound2)
     }
-   
+    var didunlockAcousticGuitar: Bool {
+        return defaults.bool(forKey: kAcousticGuitarRound1)
+    }
+    var didFinishAcousticGuitarRound1: Bool {
+        return defaults.bool(forKey: kAcousticGuitarRound1)
+    }
+    var didFinishAcousticGuitarRound2: Bool {
+        return defaults.bool(forKey: kAcousticGuitarRound2)
+    }
+    var didFinishViolinRound1: Bool {
+        return defaults.bool(forKey: kViolinRound1)
+    }
+    var didFinishViolinRound2: Bool {
+        return defaults.bool(forKey: kViolinRound2)
+    }
+    var didFinishSaxRound1: Bool {
+        return defaults.bool(forKey: kViolinRound1)
+    }
+    var didFinishSaxRound2: Bool {
+        return defaults.bool(forKey: kViolinRound2)
+    }
+    
+    //MARK: Local HighScores
     //call after completing rounds
+    
+    func setPersonalGranPianoHighScore(score: Int){
+        defaults.setValue(score, forKey: kHighScoreGrandPiano)
+        if score >= 20 {
+            unlockAcousticGuitarLocally()
+        }
+    }
+    func setPersonalAcouGuitarHighScore(score: Int){
+        defaults.setValue(score, forKey: kHighScoreAGuitar)
+        if score >= 20 {
+            unlockViolinLocally()
+        }
+    }
+    func setPersonalViolinHighScore(score: Int){
+        defaults.setValue(score, forKey: kHighScoreViolin)
+        if score >= 20 {
+            unlockSaxLocally()
+        }
+    }
+    func setPersonalSaxaphoneHighScore(score: Int){
+        defaults.setValue(score, forKey: kHighScoreSaxaphone)
+    }
+    
+    //MARK: Local Rounds
+
     func finishedRound1GrandPianoNotes(){
         defaults.setValue(true, forKey: kGrandPianoRound1)
     }
-    
     func finishedRound2GrandPianoNotes(){
         defaults.setValue(true, forKey: kGrandPianoRound2)
     }
+    func finishedRound1AcousticGuitarNotes(){
+        defaults.setValue(true, forKey: kAcousticGuitarRound1)
+    }
+    func finishedRound2AcousticGuitarNotes(){
+        defaults.setValue(true, forKey: kAcousticGuitarRound2)
+    }
+    func finishedRound1ViolinNotes(){
+        defaults.setValue(true, forKey: kViolinRound1)
+    }
+    func finishedRound2ViolinNotes(){
+        defaults.setValue(true, forKey: kViolinRound2)
+    }
+    func finishedRound1SaxNotes(){
+        defaults.setValue(true, forKey: kViolinRound1)
+    }
+    func finishedRound2SaxNotes(){
+        defaults.setValue(true, forKey: kViolinRound2)
+    }
     
+    let kIsAcousticUnlocked = "isAcousticUnlocked"
+    let kIsViolinUnlocked = "isViolinUnlocked"
+    let kIsSaxaphoneUnlocked = "isSaxaphoneUnlocked"
+
+
     let kGrandPianoRound1 = "GrandPianoRound1"
     let kGrandPianoRound2 = "GrandPianoRound2"
     
+    let kAcousticGuitarRound1 = "AcousticGuitarRound1"
+    let kAcousticGuitarRound2 = "AcousticGuitarRound2"
+    
+    let kViolinRound1 = "ViolinRound1"
+    let kViolinRound2 = "ViolinRound2"
+    
+    let kSaxRound1 = "SaxRound1"
+    let kSaxRound2 = "SaxRound2"
    
+    let kHighScoreGrandPiano = "GrandPianoHS"
+    let kHighScoreAGuitar = "AcousticGuitarHS"
+    let kHighScoreViolin = "ViolinHS"
+    let kHighScoreSaxaphone = "SaxaphoneHS"
+
+
 }
-
-
-
 
 enum LeaderboardBundleIDs: String {
     case regularGrandPiano = "com.wylan.KnowYourNote2020.leaderboards.GPNHS"
     case regularAcousticGuitar = "com.wylan.KnowYourNote2020.leaderboards.AGRHS"
+    case regularViolin = "com.wylan.KnowYourNote2020.leaderboards.VRHS"
+    case regularSaxaphone = "com.wylan.KnowYourNote2020.leaderboards.SHS"
     case advancedGrandPiano = "com.wylan.KnowYourNote2020.leaderboards.GPAHS"
     case advancedAcousticGuitar = "com.wylan.KnowYourNote2020.leaderboards.AGAHS"
+    
 }
